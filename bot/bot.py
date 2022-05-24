c67244cfdc7b3acaa8991ecf3dafe73938533255
Fallah Andy Prakasa <me@flla.my.id>
2022-05-24 16:30:41 +0700

    Add bot token

--- bot/bot.py
+++ bot/bot.py
@@ -12,18 +12,22 @@ class bot(Client):
         plugins = {'root': path.join(__package__, 'plugins')}
         api_id = config.get('pyrogram', 'api_id')
         api_hash = config.get('pyrogram', 'api_hash')
+        bot_token = config.get('pyrogram', 'bot_token')
         super().__init__(
             name,
             api_id=api_id,
             api_hash=api_hash,
+            bot_token=bot_token,
             config_file=config_file,
             workers=16,
             plugins=plugins,
             workdir="./",
         )
+
     async def start(self):
         await super().start()
         print("bot started. Hi.")
+
     async def stop(self, *args):
         await super().stop()
         print("bot stopped. Bye.")
